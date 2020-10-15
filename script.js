9d28ec8df84a5efa579a621c96bf8adc80f27c5e
Fabien <daguet77@gmail.com>
2020-10-15 15:35:47 +0200

    add score game

--- script.js
+++ script.js
@@ -3,9 +3,15 @@
 let hand = document.querySelectorAll(".js-hand");
 let hisHand = document.querySelectorAll(".js-hand-computer");
 let myChoice = document.querySelector(".js-my-choice");
+let myScore = document.querySelector(".my-score");
 let hisChoice = document.querySelector(".js-his-choice");
+let hisScore = document.querySelector(".computer-score");
+let equality = document.querySelector(".js-equal");
 let ourResult = document.querySelector(".js-result");
 let hisResult
+hisScore.textContent = 0;
+myScore.textContent = 0;
+equality.textContent = 0;
 
 function duel(event){
     let myTarget = event.currentTarget;
@@ -32,10 +38,13 @@ function duel(event){
 
 
     if (myTarget.getAttribute("data-form") === hisTarget.getAttribute("data-form")) {
+        equality.textContent++;
         ourResult.textContent = "Egalité !";
     } else if ((myTarget.getAttribute("data-form") === "scisor" && hisTarget.getAttribute("data-form") === "paper") || (myTarget.getAttribute("data-form") === "paper" && hisTarget.getAttribute("data-form") === "rock") || (myTarget.getAttribute("data-form") === "rock" && hisTarget.getAttribute("data-form") === "scisor")) {
+        myScore.textContent++;
         ourResult.textContent = "Tu as gagné !";
     } else {
+        hisScore.textContent++;
         ourResult.textContent = "Tu as perdus.";
     }
 
